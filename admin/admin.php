b59efea5739bbb991ae837dd99e46ba27c5f82ca
prive <prive@prive-G53SX>
2016-04-19 13:53:27 +0200

    Adresse du serveur

--- admin/admin.php
+++ admin/admin.php
@@ -16,7 +16,11 @@
                 <h2><a href="gestionInterro.php" >Gestion des interros </a></h2>
 
                 <h2><a href="imprimerCodesEtudiant.php" >Gestion des classes et etudiants </a></h2>
-
+<?php if ($_SERVER[ 'SERVER_ADDR'] == "127.0.0.1" || $_SERVER[ 'SERVER_ADDR'] == "127.0.1.1"){ ?>
+                <h3>Adresse à communiquer aux élèves <a href="http://www.mon-ip.com/adresse-ip-locale.php" target="blank" >sur ce site  </a></h3>
+<?php } else { ?>
+                <h3>URL à communiquer aux élèves : <a href="http://<?php echo $_SERVER[ 'SERVER_ADDR']; ?>" >http://<?php echo $_SERVER[ 'SERVER_ADDR']; ?>  </h3>
+<?php } ?>                
                 <a href="../index.php" ><img src="../img/eleve.png" alt="Retour Accueil"/></a>
 
 
