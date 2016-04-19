1043a9657e77be33dfe2d8adddc645cec0f0d6f7
prive <prive@prive-G53SX>
2016-04-19 14:14:10 +0200

    Adresse du serveur

--- admin/admin.php
+++ admin/admin.php
@@ -16,11 +16,16 @@
                 <h2><a href="gestionInterro.php" >Gestion des interros </a></h2>
 
                 <h2><a href="imprimerCodesEtudiant.php" >Gestion des classes et etudiants </a></h2>
-<?php if ($_SERVER[ 'SERVER_ADDR'] == "127.0.0.1" || $_SERVER[ 'SERVER_ADDR'] == "127.0.1.1"){ ?>
+                
+                <?php // si on fait un hébergement local 
+                if (substr($_SERVER[ 'SERVER_ADDR'], 0, 3) == "127" || substr($_SERVER[ 'SERVER_ADDR'], 0, 3) == "10" || substr($_SERVER[ 'SERVER_ADDR'], 0, 7) == "192.168" || substr($_SERVER[ 'SERVER_ADDR'], 0, 7) == "172.16"){ ?>
+
+                <?php if (substr($_SERVER[ 'SERVER_ADDR'], 0, 3) == "127"){ ?>
                 <h3>Adresse à communiquer aux élèves <a href="http://www.mon-ip.com/adresse-ip-locale.php" target="blank" >sur ce site  </a></h3>
-<?php } else { ?>
+                <?php } else { ?>
                 <h3>URL à communiquer aux élèves : <a href="http://<?php echo $_SERVER[ 'SERVER_ADDR']; ?>" >http://<?php echo $_SERVER[ 'SERVER_ADDR']; ?>  </h3>
-<?php } ?>                
+                <?php } ?>          
+                <?php } ?>          
                 <a href="../index.php" ><img src="../img/eleve.png" alt="Retour Accueil"/></a>
 
 
